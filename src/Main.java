bfd6bcee2201b58b6511c7ca336e9fe2f60f9513
Dmitry Blyukherov <d.blukherov@gmail.com>
2023-01-17 15:15:51 +0300

    Сделано ДЗ 1.5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,97 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Дмашнее задание 1.5");
+
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        boolean clientOS = false;
+        int productionYear = 2014;
+        if (clientOS) {
+            if (productionYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else if (productionYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 400;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(year + " является високосным");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int minDelivery = 1;
+        if (deliveryDistance > 100) {
+            System.out.println("Нет доставки");
+        } else {
+            if (deliveryDistance > 20) {
+                minDelivery++;
+            }
+            if (deliveryDistance > 60) {
+            minDelivery++;
+            }
+            System.out.println("Доставка займет " + minDelivery + " суток");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int monthNumber = 10;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зимний сезон");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весенний сезон");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Летний сезон");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осенний сезон");
+                break;
+
+            default:
+                System.out.println("Нет такого месяца");
+        }
     }
 }
